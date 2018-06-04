a5e5135639c1def0763f2393c1714e9d02e0323d
Leo Yassuda <leo.yassuda@gmail.com>
2018-06-04 16:26:03 -0300

    primeiro commit - criado projeto

---
+++ src/main/java/com/yassuda/intelipost/JobBackendDeveloperApplication.java
@@ -0,0 +1,12 @@
+package com.yassuda.inteliposttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBackendDeveloperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobBackendDeveloperApplication.class, args);
+    }
+}
