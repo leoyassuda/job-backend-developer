2b611ef794c0c6f2230de687f85aaf8fe08bf313
Leo Yassuda <leo.yassuda@gmail.com>
2018-06-05 12:24:24 -0300

    Corrigido nome do package.

--- src/main/java/com/yassuda/intelipost/JobBackendDeveloperApplication.java
+++ src/main/java/com/yassuda/intelipost/JobBackendDeveloperApplication.java
@@ -1,12 +1,28 @@
-package com.yassuda.inteliposttest;
+package com.yassuda.intelipost;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class JobBackendDeveloperApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JobBackendDeveloperApplication.class, args);
+    }
+
+    /**
+     * Instância de um Server de H2 para conexão remota.
+     *
+     * @return H2 Server instance
+     * @throws SQLException
+     */
+    // First App
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
     }
 }
--- src/test/java/com/yassuda/intelipost/JobBackendDeveloperApplicationTests.java
+++ src/test/java/com/yassuda/intelipost/JobBackendDeveloperApplicationTests.java
@@ -1,4 +1,4 @@
-package com.yassuda.inteliposttest;
+package com.yassuda.intelipost;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
